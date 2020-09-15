07c5baa5c25e073cee0723938f69ce978d70e9d8
tarz2929 <saitarunmulley@gmail.com>
2020-09-15 13:08:27 -0600

    Updated calculator from previous assignment

---
+++ src/components/calulator.js
@@ -0,0 +1,90 @@
+import React, { useState } from 'react'
+
+function Calculator(props) {
+
+    const [inputA, setInputA] = useState('');
+    const [inputB, setInputB] = useState('');
+    const [operator, setOperator] = useState('addition');
+    const [result, setResult] = useState('');
+    const submit = e=> { 
+        e.preventDefault();
+
+        switch(operator)
+        {
+            case "addition": 
+                setResult(Number(inputA) + Number(inputB));
+                break;
+            case "subtraction":
+                setResult(Number(inputA) - Number(inputB));
+                break;
+            case "multiplication":
+                setResult(Number(inputA) * Number(inputB));
+                break;
+            case "division":
+                setResult(Number(inputA) / Number(inputB));
+                break;
+            default:
+        }
+
+
+
+        // if (operator === "addition" )
+        // { 
+        //     setResult(Number(inputA) + Number(inputB));
+        // }
+        // else if (operator === "subtraction")
+        // {
+        //     setResult(Number(inputA) - Number(inputB));
+        // }
+        // else if (operator === "multiplication")
+        // {
+        //     setResult(Number(inputA) * Number(inputB));
+        // }
+        // else if (operator === "division")
+        // {
+        //     setResult(Number(inputA) / Number(inputB));
+        // }
+    }
+    
+    
+
+
+
+    // const newValue = useState()
+    // This.setState([this.state.name] = this.state.value);
+    //  <p attr1="1" attr2="2"> </p>
+
+ 
+    return (
+        <div className="">
+            <h1>Welcome to my Calculator !</h1>
+            <form onSubmit={submit} >
+
+                <label htmlFor="inputA">inputA:</label> 
+                <input id="inputA" type="number"  value={inputA} onChange={e => setInputA(e.target.value)}></input>              
+
+                <label htmlFor="operator">operator: </label>           
+                <select id="operator" value= { operator} onChange={e => setOperator(e.target.value)} >
+                    
+                    <option value="addition"> +</option>
+                    <option value="subtraction">- </option>
+                    <option value="multiplication">* </option>
+                    <option value="division"> / </option>
+                </select>   
+
+                <label htmlFor="inputB">inputB:</label> 
+                <input id="inputB" type="number" value={inputB} onChange={e => setInputB(e.target.value)} ></input>
+                <input type="submit"/>
+            </form>
+
+            <p>
+                {result}
+
+            </p>
+        </div>
+
+    );
+
+}
+
+export default Calculator;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Calculator from './components/calulator'
 
 
 ReactDOM.render(
   <React.StrictMode>
+    <Calculator/>
     
   </React.StrictMode>,
   document.getElementById('root')
 );
-
