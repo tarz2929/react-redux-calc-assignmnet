040e5bfc991e986feb5e13be243d6c579843464b
tarz2929 <saitarunmulley@gmail.com>
2020-09-15 13:42:28 -0600

    Create store; add subsribe for test output; ran dispatch for pre population

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Calculator from './components/calulator'
+// import Calculator from './components/calulator'
+import { createStore } from 'redux';
+import calculatorReducer from './reducers/calculator';
+import { addNewCalculator } from './actions/calculator';
+
+const store = createStore(calculatorReducer);
+
+store.subscribe( () => console.log( store.getState() ) );
+
+store.dispatch( addNewCalculator ("Research Redux") );
+store.dispatch( addNewCalculator( "Review React"));
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <Calculator/>
+    <></>
     
   </React.StrictMode>,
   document.getElementById('root')
